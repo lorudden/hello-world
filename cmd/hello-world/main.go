f06fbf60ca484bad6ed5cb8af3eca3e5d5d307c9
Isak Styf <isak.styf@masarinconsulting.se>
2023-12-16 14:55:09 +0100

    lade till lite kod för att skapa en qr kod för swish

--- cmd/hello-world/main.go
+++ cmd/hello-world/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/lorudden/hello-world/components"
+	qrcode "github.com/skip2/go-qrcode"
 )
 
 func main() {
@@ -29,7 +33,9 @@ func main() {
 		}
 	}())
 
-	r.Get("/css/{version}/tailwind.css", func() http.HandlerFunc {
+	r.Get("/api/swishqr/{size}/{phone}/{sum}/{msg}", NewSwishQRHandler())
+
+	r.Get("/css/{hash}/tailwind.css", func() http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Add("Content-Type", "text/css")
 			w.Header().Add("Cache-Control", "public,max-age=31536000,immutable")
@@ -38,7 +44,7 @@ func main() {
 		}
 	}())
 
-	r.Get("/js/{version}/htmx.min.js", func() http.HandlerFunc {
+	r.Get("/js/{hash}/htmx.min.js", func() http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Add("Content-Type", "text/css")
 			w.Header().Add("Cache-Control", "public,max-age=31536000,immutable")
@@ -74,4 +80,58 @@ func loadStaticAsset(path string) ([]byte, string, error) {
 	sha256 := fmt.Sprintf("%x", sha256.Sum256(body))
 
 	return body, sha256, nil
+}
+
+type simpleQRCode struct {
+	Content string
+	Size    int
+}
+
+func (code *simpleQRCode) generate() ([]byte, error) {
+	qrCode, err := qrcode.Encode(code.Content, qrcode.Medium, code.Size)
+	if err != nil {
+		return nil, fmt.Errorf("could not generate a QR code: %v", err)
+	}
+	return qrCode, nil
+}
+
+func NewSwishQRHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		sizeStr, _ := url.QueryUnescape(chi.URLParam(r, "size"))
+		phone, _ := url.QueryUnescape(chi.URLParam(r, "phone"))
+		sumStr, _ := url.QueryUnescape(chi.URLParam(r, "sum"))
+		msg, _ := url.QueryUnescape(chi.URLParam(r, "msg"))
+
+		size, err := strconv.Atoi(sizeStr)
+		if err != nil {
+			reportError(w, http.StatusBadRequest, "size must be an integer")
+			return
+		}
+
+		sum, err := strconv.Atoi(sumStr)
+		if err != nil {
+			reportError(w, http.StatusBadRequest, "sum must be an integer")
+			return
+		}
+
+		content := fmt.Sprintf("C%s;%d;%s;0", phone, sum, msg)
+
+		qrCode := simpleQRCode{Content: content, Size: size}
+		codeData, err := qrCode.generate()
+		if err != nil {
+			reportError(w, http.StatusBadRequest, fmt.Sprintf("could not generate QR code. %v", err))
+			return
+		}
+
+		w.Header().Set("Content-Type", "image/png")
+		w.Header().Add("Cache-Control", "public,max-age=31536000,immutable")
+		w.Write(codeData)
+	}
+}
+
+func reportError(w http.ResponseWriter, header int, msg string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(header)
+	json.NewEncoder(w).Encode(msg)
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.21.5
 require (
 	github.com/a-h/templ v0.2.476
 	github.com/go-chi/chi/v5 v5.0.10
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 )
